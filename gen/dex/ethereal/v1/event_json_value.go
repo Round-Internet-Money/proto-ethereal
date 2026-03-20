f3b13edee85b122e359390d8d24c477832d3be04
David <qiwi@roundinternet.money>
2026-03-20 16:09:02 -0700

    event-map

--- gen/dex/ethereal/v1/event_json_value.go
+++ gen/dex/ethereal/v1/event_json_value.go
@@ -1,13 +1,13 @@
 package pb
 
-var EventType_json_value = map[int32]string{
-	0: "EVENT_UNSPECIFIED",
-	1: "L2Book",
-	2: "Ticker",
-	3: "TradeFill",
-	4: "SubaccountLiquidation",
-	5: "PositionUpdate",
-	6: "OrderUpdate",
-	7: "OrderFill",
-	8: "TokenTransfer",
+var EventType_json_value = map[EventType]string{
+	EventType_EVENT_TYPE_EVENT_UNSPECIFIED:      "EVENT_UNSPECIFIED",
+	EventType_EVENT_TYPE_L2_BOOK:                "L2Book",
+	EventType_EVENT_TYPE_TICKER:                 "Ticker",
+	EventType_EVENT_TYPE_TRADE_FILL:             "TradeFill",
+	EventType_EVENT_TYPE_SUBACCOUNT_LIQUIDATION: "SubaccountLiquidation",
+	EventType_EVENT_TYPE_POSITION_UPDATE:        "PositionUpdate",
+	EventType_EVENT_TYPE_ORDER_UPDATE:           "OrderUpdate",
+	EventType_EVENT_TYPE_ORDER_FILL:             "OrderFill",
+	EventType_EVENT_TYPE_TOKEN_TRANSFER:         "TokenTransfer",
 }
